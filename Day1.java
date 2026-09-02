92bff48544f60caea1fd0feeba398be3ac2e558d
abi Mael <abimael13599@gmail.com>
2026-09-02 22:29:46 +0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,5 +1,14 @@
+//struktur dari program Java
 public class Main {
+    // ini adalah bagian utama dari program yang dimulai dari sini
     public static void main(String[] args) {
+        // untuk menampilkan hasil dari program
         System.out.println("Hello world!");
     }
 }
+// Ini adalah komentar
+
+/*
+ * Ini adalah komentar
+ * yang terdiri dari beberapa baris
+*/
