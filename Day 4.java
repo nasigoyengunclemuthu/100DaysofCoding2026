a58d28586f038a90be395a20ee88799a15639076
abi Mael <abimael13599@gmail.com>
2026-09-05 20:04:42 +0800

    Update Day 4.java

--- Day 4.java
+++ Day 4.java
@@ -1,21 +1,21 @@
 public class day4 {
-  public static void main(String[] agrs) {
-    //tipe data numerik bilangan bulat 
-    byte umur = 18;
-    short tahun = 2026;
-    int nilai = 95;
-    long manusia = 1000000000L;
+    public static void main(String[] agrs) {
+        //tipe data numerik bilangan bulat 
+        byte umur = 18;
+        short tahun = 2026;
+        int nilai = 95;
+        long manusia = 1000000000L;
 
-    // byte untuk menyimpan angka bulat yang kecil (-128 - 127) 
-    System.out.println("umur\t:" + umur);
+        // byte untuk menyimpan angka bulat yang kecil (-128 - 127) 
+        System.out.println("umur\t:" + umur);
 
-      // short untuk menyimpan angka bulat lebih besar dri byte (-32.768 - 32.767) 
-      System.out.println("tahun\t:" + tahun);
+        // short untuk menyimpan angka bulat lebih besar dri byte (-32.768 - 32.767) 
+        System.out.println("tahun\t:" + tahun);
 
-    // int ini adalah yang paling umum digunakan dalam java karna angka nya sering di pakai hari hari (-2,5m - 2,4m) 
-    System.out.println("nilai\t:" + nilai);
+        // int ini adalah yang paling umum digunakan dalam java karna angka nya sering di pakai hari hari (-2,5m - 2,4m) 
+        System.out.println("nilai\t:" + nilai);
 
-    // long untuk menyimpan angka bulat yang paling besar 
-    System.out.println("manusia\t:" + manusia);
+        // long untuk menyimpan angka bulat yang paling besar 
+        System.out.println("manusia\t:" + manusia);
   }
 }
