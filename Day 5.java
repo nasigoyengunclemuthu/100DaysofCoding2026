7f76d054cc2c8a25d5cf62b17eac6b1c6e033c70
abi Mael <abimael13599@gmail.com>
2026-09-06 15:50:05 +0800

    Create Day 5.java

---
+++ Day 5.java
@@ -0,0 +1,11 @@
+public class day5 {
+  public static void main(String[] args) {
+    //float, jika mau pke float harus ad f di belakang angka 
+    Float tinggibadan =165.1f;
+    //double lebih teliti dari pada float dan sering jg di gunakan dalam java 
+    Double hutang =120000.01;
+
+    System.out.println("tinggi badan:"+tinggibadan);
+    System.out.println("hutang:"+hutang);
+  }
+}
