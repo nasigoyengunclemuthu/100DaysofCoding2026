57baaa0dd6679aadcfcf492cd9f1c17ebdc1479e
abi Mael <abimael13599@gmail.com>
2026-09-07 21:36:42 +0800

    Create Day 6.java

---
+++ Day 6.java
@@ -0,0 +1,23 @@
+public class day6{
+  public static void main(String[]args ){
+
+    //char menggunakan tanda petik 1 '@'
+    char huruf = 'A';
+    char angka = '1';
+    char simbol = '@';
+
+    //boolean itu tipe data java yang mempunyai 2 perintah true dan false
+    boolean sudahlulus = true;
+    boolean sudahhujan = false;
+
+    
+    System.out.println("huruf: " + huruf);
+    System.out.println("angka: " + angka);
+    System.out.println("simbol: " + simbol);
+
+      
+    System.out.println("sudah lulus   :" + sudahlulus);
+    System.out.println("sudah hujan    :" + sudahhujan);
+  
+  }
+}
