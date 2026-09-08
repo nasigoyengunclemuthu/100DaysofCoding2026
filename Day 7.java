c39b85a4ed13d2dbe4f1e3c85a8bd7d261a4e5d1
abi Mael <abimael13599@gmail.com>
2026-09-08 18:41:47 +0800

    Update Day 7.java

--- Day 7.java
+++ Day 7.java
@@ -1,8 +1,8 @@
 public class day7 {
   public static void main(String[]args ) {
     //String untuk menyimpan teks
-    String nama = ("abi mael al rajab");
-    String prodi = ("informatika");
+    String nama = "abi mael al rajab";
+    String prodi = "informatika";
 
     System.out.println ("nama\t:" + nama);
     System.out.println ("prodi\t;"+ prodi);
