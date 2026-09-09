0790721efda0995387f444926e8a07a09ae455d5
abi Mael <abimael13599@gmail.com>
2026-09-09 18:33:06 +0800

    Create Day 8.java

---
+++ Day 8.java
@@ -0,0 +1,14 @@
+public class main {
+  public static void main (String[]args ) {
+// deklarasi
+  int umur;
+//inisialisasi
+  umur = 17;
+
+  System.out.println("umur awal:  " + umur);
+// update
+  umur = 18;
+
+  System.out.println("umur baru:  " + umur);
+  }
+}
