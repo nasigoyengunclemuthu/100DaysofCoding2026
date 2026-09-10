ea3c8b7c5b7bc4739da7dddde587de09224c9d6c
abi Mael <abimael13599@gmail.com>
2026-09-10 18:08:17 +0800

    Create Day 9.java

---
+++ Day 9.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    //konstanta variabel yg tidak bisa di ganti
+        final int UMUR = 18;
+
+        System.out.println("umur: " + UMUR);
+    }
+}
