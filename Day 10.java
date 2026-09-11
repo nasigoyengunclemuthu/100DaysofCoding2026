4351a003c617982ee341aa2b9d8f24466a8c6a43
abi Mael <abimael13599@gmail.com>
2026-09-11 20:44:32 +0800

    Update Day 10.java

--- Day 10.java
+++ Day 10.java
@@ -1,1 +1,34 @@
+public class day10 {
+  public static void main(String[]args ) {
+    //Byte
+    System.out.println("--byte--");
+    System.out.println("MIN\t:" + Byte.MIN_VALUE);
+    System.out.println("MAX\t:" + Byte.MAX_VALUE);
+    //short
+    System.out.println("--short--");
+    System.out.println("MIN\t:" + Short.MIN_VALUE);
+    System.out.println("MAX\t:" + Short.MAX_VALUE);
+    //int
+    System.out.println("--int--");
+    System.out.println("MIN\t:" + Integer.MIN_VALUE);
+    System.out.println("MAX\t:" + Integer.MAX_VALUE);
+    //long
+    System.out.println("--long--");
+    System.out.println("MIN\t:" + Long.MIN_VALUE);
+    System.out.println("MAX\t:" + Long.MAX_VALUE);
+    //float
+    System.out.println("--float--");
+    System.out.println("MIN\t:" + Float.MIN_VALUE);
+    System.out.println("MAX\t:" + Float.MAX_VALUE);
+    //double
+    System.out.println("--double--");
+    System.out.println("MIN\t:" + Double.MIN_VALUE);
+    System.out.println("MAX\t:" + Double.MAX_VALUE);
+    //char
+    System.out.println("--char--");
+    System.out.println("MIN\t:" + (int) Character.MIN_VALUE);
+    System.out.println("MAX\t:" + (int) Character.MAX_VALUE);
+    //boolean tidak punya min sma max
+  }
+}
 
