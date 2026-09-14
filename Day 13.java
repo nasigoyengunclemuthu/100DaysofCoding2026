bf3cbb9392a10b3974ff2b42cfa8f6b44d275a9b
abi Mael <abimael13599@gmail.com>
2026-09-14 22:08:09 +0800

    Update Day 13.java

--- Day 13.java
+++ Day 13.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
       Scanner in=new Scanner (System.in);
       String nama = in.nextLine();
       int umur = in.nextInt();
-      in.nextInt();
+      in.nextLine();
       String hobi = in.nextLine();
       double ipk = in.nextDouble();
 
-      System.out.printf("Nama saya %s umur saya %d dan saya hobi %s , makanya ipk sya %f",nama,umur,hobi,ipk);
+      System.out.printf("Nama saya %s umur saya %d dan saya hobi %s , makanya ipk sya %.2f",nama,umur,hobi,ipk);
       
     }
   }
