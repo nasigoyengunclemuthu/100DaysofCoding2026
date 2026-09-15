e6ed4d427d40ef8f3501fbac9f1fab7dfa0b94ac
abi Mael <abimael13599@gmail.com>
2026-09-15 21:33:48 +0800

    Create Day 14.java

---
+++ Day 14.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Penjumlahan : " + (a + b));
+        System.out.println("Pengurangan : " + (a - b));
+    }
+}
