1e896b1a281f65d74667e9f47ce563b13794e48a
abi Mael <abimael13599@gmail.com>
2026-09-18 19:24:00 +0800

    Create Day 17.java

---
+++ Day 17.java
@@ -0,0 +1,20 @@
+public class day17{
+  public static void main (String[]args ){
+    int a =10;
+    a += 5;
+    int b=10;
+    b -= 4;
+    int c = 10;
+    c *= 3;
+    int d=10;
+    d /=2;
+    int e=10;
+    e %= 1;
+
+    System.out.println("tambah:"+a);
+    System.out.println("kurang:"+b);
+    System.out.println("kali:"+c);
+    System.out.println("bagi:"+d);
+    System.out.println("sisabagi:"+e);
+  }
+}
