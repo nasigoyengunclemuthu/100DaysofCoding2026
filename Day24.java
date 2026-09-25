e4de9e55f70fc3c358b13422822663388ca59014
abi Mael <abimael13599@gmail.com>
2026-09-25 20:04:29 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang: ");
+        int panjang = input.nextInt();
+
+        System.out.print("Masukkan lebar: ");
+        int lebar = input.nextInt();
+
+        int luas = panjang * lebar;
+
+        System.out.println("Luas persegi panjang: " + luas);
+
+        input.close();
+    }
+}
